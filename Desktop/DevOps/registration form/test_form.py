42298e4e212506b794e503edca0d1cafc11801e1
Shlok Anand <152406547+ShlokAnand@users.noreply.github.com>
2026-03-24 11:14:23 +0530

    Update test_form.py

--- Desktop/DevOps/registration form/test_form.py
+++ Desktop/DevOps/registration form/test_form.py
@@ -6,7 +6,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 options = Options()
-options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
